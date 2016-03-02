0b29d7603c5bd98da42e91933b7e6750708a59f8
Sameer Kulkarni <ksameersrk@gmail.com>
2016-03-02 22:13:23 +0530

    main window panel

---
+++ front_end/app/src/main/java/com/example/jeevan/splash/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.jeevan.splash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import static com.example.jeevan.splash.R.layout.splashh;
+
+/**
+
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(splashh);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
